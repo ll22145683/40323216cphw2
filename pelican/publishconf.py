93d7211e8196004aa1f8d1bd45932b46656773c5
40323216 <ll22145683@gmail.com>
2015-11-16 14:50:52 +0800

    40323216

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),)
+         ('Python', 'http://python.org/'),('班級作業', 'http://2015fallhw.github.io/2015fallcadpb/user/40323216/'),)
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
